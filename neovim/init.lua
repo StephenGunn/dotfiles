0ea51fd0960e111fc75915932ef9500d5da9ec66
Stephen <stephengunn@gmail.com>
2024-05-13 14:43:32 -0500

    nvim lsps pretty good

---
+++ neovim/init.lua
@@ -0,0 +1,20 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- vim options are in /luA/vim-options.lua -- needs to be before plugins require
+require("vim-options")
+-- auto load plugins from /lua/plugins folder
+require("lazy").setup("plugins")
+
+
+
