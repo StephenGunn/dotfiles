6dc4b7609c19f24c4f75c9cce614eecff6c6221b
Stephen <stephengunn@gmail.com>
2024-05-21 10:48:09 -0500

    still working..

--- neovim/lua/plugins/lsp-config.lua
+++ neovim/lua/plugins/lsp-config.lua
@@ -3,39 +3,79 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "tsserver"}
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "jsonls",
+          "emmet_ls",
+          "html",
+          "cssls",
+        },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.tsserver.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "html",
+          "typescriptreact",
+          "javascriptreact",
+          "css",
+          "sass",
+          "svelte",
+          "scss",
+          "less",
+          "javascript",
+          "typescript",
+          "vue",
+        },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
+
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {})
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-
-    end
-  }
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- neovim/lua/vim-options.lua
+++ neovim/lua/vim-options.lua
@@ -9,7 +9,7 @@ vim.cmd("set autoindent")
 vim.cmd("set smartindent")
 
 -- basic vim options
-vim.g.mapleader  = " "
+vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
@@ -17,16 +17,45 @@ vim.opt.signcolumn = "number"
 -- add clipboard support
 vim.opt.clipboard = "unnamedplus"
 
+-- change the active line number color to something a little brighter
+vim.o.cursorline = true
+vim.cmd([[highlight CursorLineNr guifg=#5c53bd guibg=NONE]])
+
+-- Delete word without adding to register
+vim.api.nvim_set_keymap("n", "<leader>d", '"_d', { silent = true })
+
+-- Delete line without adding to register
+vim.api.nvim_set_keymap("n", "<leader>dd", '"_dd', { silent = true })
+
+-- Emmet configuration
+vim.cmd([[
+let g:user_emmet_mode='i'
+let g:user_emmet_leader_key=','
+]])
+
+-- Rebind Ctrl + PageDown to move the current buffer to a new vertical split and close the original buffer
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-PageDown>",
+  ":vsplit<CR>:wincmd l<CR>:b #<CR>:wincmd q<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Rebind Ctrl + PageUp to move the current buffer to the left split
+vim.api.nvim_set_keymap("n", "<C-PageUp>", ":wincmd h<CR>", { noremap = true, silent = true })
+
+-- delete the selected text and paste, where you don't lose your copied text
+vim.keymap.set("n", "<leader>p", '"_dP')
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- Jump between buffers
-vim.keymap.set('n', '<S-l>', ':BufferLineCycleNext<CR>')
-vim.keymap.set('n', '<S-h>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>")
 
 -- Visualize space character in the indent
 vim.cmd("set list")
@@ -38,26 +67,20 @@ vim.g.terminal = "tmux-256color"
 vim.o.termguicolors = true
 
 -- Set the highlight color for function scopes
-vim.api.nvim_set_hl(0, 'TSFunction', { fg = '#FFD700', bg = 'NONE', bold = true })
+vim.api.nvim_set_hl(0, "TSFunction", { fg = "#FFD700", bg = "NONE", bold = true })
 
 -- Set the highlight color for method scopes
-vim.api.nvim_set_hl(0, 'TSMethod', { fg = '#ADFF2F', bg = 'NONE', bold = true })
+vim.api.nvim_set_hl(0, "TSMethod", { fg = "#ADFF2F", bg = "NONE", bold = true })
 
 -- Set the highlight color for parameter scopes
-vim.api.nvim_set_hl(0, 'TSParameter', { fg = '#1E90FF', bg = 'NONE', italic = true })
-
+vim.api.nvim_set_hl(0, "TSParameter", { fg = "#1E90FF", bg = "NONE", italic = true })
 
 -- Change the color of the indent character
 vim.cmd("highlight IndentChar guifg=#4b5263")
 
-
 -- Move lines up and down
-vim.keymap.set('n', '<S-j>', ':m .+1<CR>==')
-vim.keymap.set('n', '<S-k>', ':m .-1<CR>==')
+vim.keymap.set("n", "<S-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<S-k>", ":m .-1<CR>==")
 
 -- Clear search highlights
-vim.keymap.set('n', '<leader>c', ':nohlsearch<CR>')
-
-
-
-
+vim.keymap.set("n", "<leader>c", ":nohlsearch<CR>")
