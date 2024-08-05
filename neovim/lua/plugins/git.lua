793f231d0b352b975f1bd1a7c4393c9f92322d40
Stephen Gunn <stephengunn@gmail.com>
2024-08-04 20:12:48 -0500

    removed vim fugative for neogit

--- neovim/lua/plugins/git.lua
+++ neovim/lua/plugins/git.lua
@@ -1,16 +1,24 @@
 return {
-  {
-    "tpope/vim-fugitive",
-    event = "VeryLazy",
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("gitsigns").setup()
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame <CR>")
-    end,
-  },
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame <CR>")
+		end,
+	},
 }
