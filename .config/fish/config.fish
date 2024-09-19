9e9ef2fba028d44d8afdb44d1b89e5f63fb805a7
Stephen Gunn <stephengunn@gmail.com>
2024-09-18 23:26:39 -0500

    awesome prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,15 @@ starship init fish | source
 set -U fish_greeting "🌎 🚀    🪐"
 
 source ~/.config/fish/aliases.fish
+
+# This function shows the vi mode dynamically in the right prompt
+function fish_right_prompt
+    switch $fish_bind_mode
+        case insert
+            echo -n (set_color green)" INSERT "(set_color normal)
+        case default
+            echo -n (set_color red)" NORMAL "(set_color normal)
+        case visual
+            echo -n (set_color yellow)" VISUAL "(set_color normal)
+    end
+end
