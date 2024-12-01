3056b4db77496f0508871658c4e0b998e2a71181
Stephen Gunn <stephengunn@gmail.com>
2024-11-30 18:18:09 -0600

    dots

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -44,3 +44,13 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
