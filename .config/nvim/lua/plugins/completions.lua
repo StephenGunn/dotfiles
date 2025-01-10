5a475e7c993da3ddfa11b812eba34f1cc5c58d58
Stephen Gunn <stephengunn@gmail.com>
2025-01-10 16:44:28 -0600

    moved copilot to suggestions

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,98 +1,100 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-      "nvim-lua/plenary.nvim"
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "onsails/lspkind.nvim",
-      "windwp/nvim-ts-autotag",
-      "windwp/nvim-autopairs",
-    },
-    config = function()
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      require("luasnip").config.set_config({
-        -- Example config settings
-        history = true,
-        updateevents = "TextChanged,TextChangedI",
-        enable_autosnippets = true,
-      })
-      require("luasnip.loaders.from_vscode").lazy_load()
-      local lspkind = require("lspkind")
-      local cmp = require("cmp")
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"onsails/lspkind.nvim",
+			"windwp/nvim-ts-autotag",
+			"windwp/nvim-autopairs",
+		},
+		config = function()
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			require("luasnip").config.set_config({
+				-- Example config settings
+				history = true,
+				updateevents = "TextChanged,TextChangedI",
+				enable_autosnippets = true,
+			})
+			require("luasnip.loaders.from_vscode").lazy_load()
+			local lspkind = require("lspkind")
+			local cmp = require("cmp")
 
-      -- Load snippets
+			-- Load snippets
 
-      -- Integrate nvim-autopairs with cmp
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			-- Integrate nvim-autopairs with cmp
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-      require("nvim-autopairs").setup()
-      require("luasnip.loaders.from_vscode").lazy_load()
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
+			require("nvim-autopairs").setup()
+			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
 
-      cmp.setup({
-        enabled = function()
-          -- disable completion in comments
-          local context = require("cmp.config.context")
-          -- keep command mode completion enabled when cursor is in a comment
-          if vim.api.nvim_get_mode().mode == "c" then
-            return true
-          else
-            return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-          end
-        end,
+			cmp.setup({
+				enabled = function()
+					-- disable completion in comments
+					local context = require("cmp.config.context")
+					-- keep command mode completion enabled when cursor is in a comment
+					if vim.api.nvim_get_mode().mode == "c" then
+						return true
+					else
+						return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+					end
+				end,
 
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        -- sources for autocompletion
-        sources = cmp.config.sources({
-          { name = "nvim_lsp",               group_index = 1 },                     -- lsp
-          { name = "buffer",                 max_item_count = 5, group_index = 2 }, -- text within current buffer
-          { name = "path",                   max_item_count = 3, group_index = 3 }, -- file system paths
-          { name = "luasnip",                max_item_count = 3, group_index = 5 }, -- snippets
-          { name = "nvim-lsp-signature-help" },
-        }),
-        -- Enable pictogram icons for lsp/autocompletion
-        formatting = {
-          expandable_indicator = true,
-          format = lspkind.cmp_format({
-            mode = "symbol_text",
-            maxwidth = 50,
-            ellipsis_char = "...",
-            menu = {
-              nvim_lsp = "[LSP]",
-              buffer = "[Buffer]",
-              path = "[PATH]",
-              luasnip = "[LuaSnip]",
-            },
-          }),
-        },
-      })
-    end,
-  },
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				-- sources for autocompletion
+				sources = cmp.config.sources({
+					{ name = "copilot", group_index = 1 },
+					{ name = "nvim_lsp", group_index = 1 }, -- lsp
+					{ name = "buffer", max_item_count = 5, group_index = 2 }, -- text within current buffer
+					{ name = "path", max_item_count = 3, group_index = 3 }, -- file system paths
+					{ name = "luasnip", max_item_count = 3, group_index = 5 }, -- snippets
+					{ name = "nvim-lsp-signature-help" },
+				}),
+				-- Enable pictogram icons for lsp/autocompletion
+				formatting = {
+					expandable_indicator = true,
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						maxwidth = 50,
+						ellipsis_char = "...",
+						menu = {
+							copilot = "[Copilot]",
+							nvim_lsp = "[LSP]",
+							buffer = "[Buffer]",
+							path = "[PATH]",
+							luasnip = "[LuaSnip]",
+						},
+					}),
+				},
+			})
+		end,
+	},
 }
