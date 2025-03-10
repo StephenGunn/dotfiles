1812b50ab3aed3ff0316882e82be99a2a182d503
Stephen Gunn <stephengunn@gmail.com>
2025-03-10 15:16:00 -0500

    nvim svelte dotfiles

--- .config/nvim/lua/plugins/svelte_check.lua
+++ .config/nvim/lua/plugins/svelte_check.lua
@@ -1,11 +1,12 @@
 return {
-	"StephenGunn/sveltecheck.nvim",
+	dir = "~/projects/nvim-svelte-check",
 	branch = "main",
 	config = function()
-		local sc = require("sveltecheck")
+		local sc = require("svelte-check")
 		sc.setup({
 			command = "pnpm run check",
-			debug_mode = true,
+			debug_mode = false,
 		})
 	end,
+	dev = true,
 }
