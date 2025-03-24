c349f09bf14d1a522b208ffdda3be838e5856d80
Stephen Gunn <stephengunn@gmail.com>
2025-03-24 17:56:43 -0500

    yes

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,249 +1,248 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"jsonls",
-					"emmet_ls",
-					"html",
-					"cssls",
-					"svelte",
-					"ocamllsp",
-					"somesass_ls",
-					"elixirls",
-					"rust_analyzer",
-					"gopls",
-					"marksman",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "jsonls",
+          "emmet_ls",
+          "html",
+          "cssls",
+          "svelte",
+          "ocamllsp",
+          "somesass_ls",
+          "elixirls",
+          "rust_analyzer",
+          "gopls",
+          "marksman",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						runtime = {
-							-- Tell the language server which version of Lua you're using
-							-- (most likely LuaJIT in the case of Neovim)
-							version = "LuaJIT",
-						},
-						diagnostics = {
-							-- Get the language server to recognize the `vim` global
-							globals = {
-								"vim",
-								"require",
-							},
-						},
-						workspace = {
-							-- Make the server aware of Neovim runtime files
-							library = vim.api.nvim_get_runtime_file("", true),
-						},
-						-- Do not send telemetry data containing a randomized but unique identifier
-						telemetry = {
-							enable = false,
-						},
-					},
-				},
-			})
-			lspconfig.somesass_ls.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"sass",
-				},
-			})
-			lspconfig.ocamllsp.setup({
-				cmd = { "ocamllsp" },
-				filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
-				root_dir = lspconfig.util.root_pattern(
-					"*.opam",
-					"esy.json",
-					"package.json",
-					".git",
-					"dune-project",
-					"dune-workspace"
-				),
-				capabilities = capabilities,
-			})
-			lspconfig.marksman.setup({
-				capabilities = capabilities,
-				filetypes = { "markdown", "markdown.mdx" },
-			})
-			lspconfig.gleam.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"html",
-					"liquid",
-				},
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-				settings = {
-					css = {
-						validate = true,
-						lint = {
-							unknownAtRules = "ignore",
-						},
-					},
-					scss = {
-						validate = true,
-						lint = {
-							unknownAtRules = "ignore",
-						},
-					},
-					less = {
-						validate = true,
-						lint = {
-							unknownAtRules = "ignore",
-						},
-					},
-				},
-			})
-			-- TypeScript LSP with enhanced settings for interfaces and type information
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				-- Make sure lsp name is "tsserver" not "ts_ls" (which might be a custom alias)
-				filetypes = {
-					"typescript",
-					"typescriptreact",
-					"typescript.tsx",
-					"javascript",
-					"javascriptreact",
-					"javascript.jsx",
-				},
-				settings = {
-					typescript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "all",
-							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = true,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-							includeInlayEnumMemberValueHints = true,
-						},
-						implementationsCodeLens = {
-							enabled = true,
-						},
-						referencesCodeLens = {
-							enabled = true,
-						},
-						preferences = {
-							quoteStyle = "double",
-						},
-						suggest = {
-							completeFunctionCalls = true,
-						},
-					},
-					javascript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "all",
-							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = true,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-						},
-					},
-				},
-				commands = {
-					OrganizeImports = {
-						function()
-							vim.lsp.buf.execute_command({
-								command = "_typescript.organizeImports",
-								arguments = { vim.api.nvim_buf_get_name(0) },
-							})
-						end,
-						description = "Organize Imports",
-					},
-				},
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {
+                "vim",
+                "require",
+              },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+      lspconfig.somesass_ls.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "sass",
+        },
+      })
+      lspconfig.ocamllsp.setup({
+        cmd = { "ocamllsp" },
+        filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+        root_dir = lspconfig.util.root_pattern(
+          "*.opam",
+          "esy.json",
+          "package.json",
+          ".git",
+          "dune-project",
+          "dune-workspace"
+        ),
+        capabilities = capabilities,
+      })
+      lspconfig.marksman.setup({
+        capabilities = capabilities,
+        filetypes = { "markdown", "markdown.mdx" },
+      })
+      lspconfig.gleam.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "html",
+          "liquid",
+        },
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+        settings = {
+          css = {
+            validate = true,
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+          scss = {
+            validate = true,
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+          less = {
+            validate = true,
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+        },
+      })
+      -- TypeScript LSP
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+        },
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+            implementationsCodeLens = {
+              enabled = true,
+            },
+            referencesCodeLens = {
+              enabled = true,
+            },
+            preferences = {
+              quoteStyle = "double",
+            },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+            },
+          },
+        },
+        commands = {
+          OrganizeImports = {
+            function()
+              vim.lsp.buf.execute_command({
+                command = "_typescript.organizeImports",
+                arguments = { vim.api.nvim_buf_get_name(0) },
+              })
+            end,
+            description = "Organize Imports",
+          },
+        },
+      })
 
-			-- Svelte LSP
-			local svelte_lsp_capabilities = vim.tbl_deep_extend("force", {}, capabilities)
-			svelte_lsp_capabilities.workspace = { didChangeWatchedFiles = false }
-			lspconfig.svelte.setup({
-				capabilities = svelte_lsp_capabilities,
-				filetypes = { "svelte" },
-				settings = {
-					svelte = {
-						plugin = {
-							svelte = {
-								defaultScriptLanguage = "ts",
-							},
-						},
-					},
-				},
-			})
+      -- Svelte LSP
+      local svelte_lsp_capabilities = vim.tbl_deep_extend("force", {}, capabilities)
+      svelte_lsp_capabilities.workspace = { didChangeWatchedFiles = false }
+      lspconfig.svelte.setup({
+        capabilities = svelte_lsp_capabilities,
+        filetypes = { "svelte" },
+        settings = {
+          svelte = {
+            plugin = {
+              svelte = {
+                defaultScriptLanguage = "ts",
+              },
+            },
+          },
+        },
+      })
 
-			lspconfig.emmet_ls.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"html",
-					"css",
-					"svelte",
-					"scss",
-					"less",
-				},
-				init_options = {
-					html = {
-						options = {
-							["bem.enabled"] = true,
-							["output.inlineBreak"] = 1,
-						},
-					},
-				},
-			})
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "html",
+          "css",
+          "svelte",
+          "scss",
+          "less",
+        },
+        init_options = {
+          html = {
+            options = {
+              ["bem.enabled"] = true,
+              ["output.inlineBreak"] = 1,
+            },
+          },
+        },
+      })
 
-			-- Elixir LSP configuration
-			lspconfig.elixirls.setup({
-				capabilities = capabilities,
-				cmd = { "elixir-ls" },
-				on_attach = function(client)
-					if client.server_capabilities.documentFormattingProvider then
-						vim.cmd([[
+      -- Elixir LSP
+      lspconfig.elixirls.setup({
+        capabilities = capabilities,
+        cmd = { "elixir-ls" },
+        on_attach = function(client)
+          if client.server_capabilities.documentFormattingProvider then
+            vim.cmd([[
             augroup LspFormatting
               autocmd! * <buffer>
               autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
             augroup END
           ]])
-					end
-				end,
-			})
+          end
+        end,
+      })
 
-			-- Additional keybindings for LSP
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      -- Additional keybindings
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
